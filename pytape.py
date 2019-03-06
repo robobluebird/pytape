22a4a9db041c87010176237719b3105acf0deb36
robobluebird <schroza@gmail.com>
2019-03-06 20:48:44 +0000

    make it a class

--- pytape.py
+++ pytape.py
@@ -11,101 +11,102 @@ from gpiozero import Button
 from signal import pause
 
 import subprocess
+import requests
 
-b1 = Button('GPIO21')
-b2 = Button('GPIO20')
-b3 = Button('GPIO16')
-b4 = Button('GPIO12')
+class Web:
+    def check_uploads():
+        r = requests.get('')
 
-menu_index = 0
-menu_length = 3
-menu_items = ["first", "second", "third", "fourth"]
-menu_range_start = 0
-menu_range_end = 2
+        print(r.status_code)
+        print(r.json())
 
-def menu_up():
-    global menu_index
+class PyTape:
+    def __init__(self):
+        self.menu_items = ['check for sounds', 'play from start', 'tape control']
+        self.menu_index = 0
+        self.menu_length = 3
+        self.menu_range_start = 0
+        self.menu_range_end = 2
 
-    if menu_index > 0:
-        menu_index -= 1
-        draw_menu()
+        self.b1 = Button('GPIO21')
+        self.b2 = Button('GPIO20')
+        self.b3 = Button('GPIO16')
+        self.b4 = Button('GPIO12')
 
-def menu_down():
-    global menu_index
+        self.display = Adafruit_SSD1306.SSD1306_128_32(rst=None)
 
-    if menu_index < len(menu_items) - 1:
-        menu_index += 1
-        draw_menu()
+        self.display.begin()
+        self.display.clear()
+        self.display.display()
 
-def menu_in():
-    obj = globals(menu_items[menu_index])
+        # Some other nice fonts to try: http://www.dafont.com/bitmap.php
+        # font = ImageFont.truetype('Minecraftia.ttf', 8)
+        self.font = ImageFont.load_default()
 
-    if callable(obj):
-        obj()
+        # Create blank image for drawing.
+        # Make sure to create image with mode '1' for 1-bit color.
+        self.width = self.display.width
+        self.height = self.display.height
+        self.image = Image.new('1', (self.width, self.height))
 
-def menu_out():
-    print('blep')
+        # Get drawing object to draw on image.
+        self.draw = ImageDraw.Draw(self.image)
 
-def menu():
-    menu_index = 0
-    b1.when_pressed = menu_up
-    b2.when_pressed = menu_down
-    b3.when_pressed = menu_out
-    b4.when_pressed = menu_in
-    draw_menu()
+        # Draw a black filled box to clear the image.
+        self.draw.rectangle((0, 0, self.width, self.height), outline = 0, fill = 0)
 
-def draw_menu():
-    global menu_range_start
-    global menu_range_end
+        cmd = "iwgetid -r"
+        ssid = subprocess.check_output(cmd, shell = True )
+        self.draw.text((0, -2), str(ssid), font=self.font, fill=255)
 
-    if menu_index < menu_range_start:
-        menu_range_start = menu_index
-        menu_range_end = menu_index + 2
-    elif menu_index > menu_range_end:
-        menu_range_start = menu_index - 2
-        menu_range_end = menu_index
+    def menu_up(self):
+        if self.menu_index > 0:
+            self.menu_index -= 1
+            self.draw_menu()
 
-    draw.rectangle((0, 8, width, height), outline = 0, fill = 0)
+    def menu_down(self):
+        if self.menu_index < len(self.menu_items) - 1:
+            self.menu_index += 1
+            self.draw_menu()
 
-    menu_item_y = 6
+    def menu_in(self):
+        obj = self.menu_items[menu_index]
 
-    for idx in range(menu_range_start, menu_range_end + 1):
-        leader = "-> " if idx == menu_index else "   "
-        draw.text((0, menu_item_y), leader + menu_items[idx], font=font, fill=255)
-        menu_item_y += 8
+        if callable(obj):
+            obj()
 
-    display.image(image)
-    display.display()
+    def menu_out(self):
+        print('blep')
 
-display = Adafruit_SSD1306.SSD1306_128_32(rst=None)
+    def menu(self):
+        self.menu_index = 0
+        self.b1.when_pressed = self.menu_up
+        self.b2.when_pressed = self.menu_down
+        self.b3.when_pressed = self.menu_out
+        self.b4.when_pressed = self.menu_in
+        self.draw_menu()
 
-display.begin()
-display.clear()
-display.display()
+    def draw_menu(self):
+        if self.menu_index < self.menu_range_start:
+            self.menu_range_start = self.menu_index
+            self.menu_range_end = self.menu_index + 2
+        elif self.menu_index > self.menu_range_end:
+            self.menu_range_start = self.menu_index - 2
+            self.menu_range_end = self.menu_index
 
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
-# font = ImageFont.truetype('Minecraftia.ttf', 8)
-font = ImageFont.load_default()
+        self.draw.rectangle((0, 8, self.width, self.height), outline = 0, fill = 0)
 
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = display.width
-height = display.height
-image = Image.new('1', (width, height))
+        self.menu_item_y = 6
 
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
+        for idx in range(self.menu_range_start, self.menu_range_end + 1):
+            leader = "-> " if idx == self.menu_index else "   "
+            self.draw.text((0, self.menu_item_y), leader + self.menu_items[idx], font=self.font, fill=255)
+            self.menu_item_y += 8
 
-# Draw a black filled box to clear the image.
-draw.rectangle((0, 0, width, height), outline = 0, fill = 0)
+        self.display.image(self.image)
+        self.display.display()
 
-cmd = "iwgetid -r"
-ssid = subprocess.check_output(cmd, shell = True )
-draw.text((0, -2), str(ssid), font=font, fill=255)
-
-menu()
-
-display.image(image)
-display.display()
-
-pause()
+if __name__ == "__main__":
+    pytape = PyTape()
+    pytape.menu()
+    pause()
